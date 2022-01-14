5605efcdb77d9c8338908111633236f96b675d8c
Samantha Diaz <samidiaz3@gmail.com>
2022-01-14 10:52:35 -0500

    commit tree 75ab0c41210480a841c6fec5672da5b670297de0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// const namesArray = ["Ada", "Brendan", "Ali"]
+
+// for loop
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+// while loop
+
+function countDown( startingNumber ) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+    console.log(startingNumber);
+}
